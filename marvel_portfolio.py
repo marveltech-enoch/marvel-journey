7b9fa3970cfe644229e620b3f4a4499693bf64f1
marveltech-enoch <marvelousenoch160@gmail.com>
2026-06-23 20:05:58 +0100

    Add files via upload

---
+++ marvel_portfolio.py
@@ -0,0 +1,28 @@
+founder = "Abioye Marvelous Olaitan Enoch"
+age = 18
+location = "Nigeria"
+yrs_of_experience = 0
+projects_built = 0
+github_followers = 0
+revenue_generated = 0
+target = "1,000,000,000"
+timeline = "5 Years"
+status = "IN PROGRESS🔥"
+
+print("=====================================")
+print("        MARVEL TECHNOLOGIES          ")
+print("=====================================")
+print("Founder:",founder)
+print("Age:", age)
+print("Location:",location)
+print("CURRENT STATS:")
+print("-Years of Experience:", yrs_of_experience)
+print("-Projects Built:", projects_built)
+print("-GitHub Followers:", github_followers)
+print("-Revenue Generated:$",revenue_generated)
+
+print("Target:", target)
+print("Timeline:", timeline)
+print("Status:", status)
+
+print("=====================================")
